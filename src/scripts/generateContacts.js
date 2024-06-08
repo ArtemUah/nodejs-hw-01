8e08dc958f75605f3b43c91cdbdd65b44ef3a418
Artem <111749288+ArtemUah@users.noreply.github.com>
2024-06-08 13:21:22 +0300

    first task

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,7 +1,21 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
+import { createFakeContact } from '../utils/createFakeContact.js';
+
 
 
 const generateContacts = async (number) => {
+try {
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    const arrOfContacts = JSON.parse(data);
+    for (let i = 0; i < number; i++) {
+        arrOfContacts.push(createFakeContact());
+    };
+    await fs.writeFile(PATH_DB, JSON.stringify(arrOfContacts), 'utf-8')
+    console.log(arrOfContacts);
+} catch (error) {
+    console.log('Sudden error:', error)
+}
 };
 
-await generateContacts(5);
+await generateContacts(3);
