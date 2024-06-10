6f149bb1fd7222372c9d691cfa00e1c14a707988
Artem <111749288+ArtemUah@users.noreply.github.com>
2024-06-10 13:53:22 +0300

    three tasks

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,5 +1,15 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
 export const addOneContact = async () => {};
+try {
+const contacts = await fs.readFile(PATH_DB, 'utf-8');
+const arrOfContacts = JSON.parse(contacts);
+arrOfContacts.push(createFakeContact());
+await fs.writeFile(PATH_DB, JSON.stringify(arrOfContacts), 'utf-8');
+} catch (error) {
+    console.log('Sudden error:', error);
+}
 
 await addOneContact();
--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -2,20 +2,17 @@ import { PATH_DB } from '../constants/contacts.js';
 import fs from 'fs/promises';
 import { createFakeContact } from '../utils/createFakeContact.js';
 
-
-
 const generateContacts = async (number) => {
-try {
-    const data = await fs.readFile(PATH_DB, 'utf-8');
-    const arrOfContacts = JSON.parse(data);
+  try {
+    const contacts = await fs.readFile(PATH_DB, 'utf-8');
+    const arrOfContacts = JSON.parse(contacts);
     for (let i = 0; i < number; i++) {
-        arrOfContacts.push(createFakeContact());
-    };
-    await fs.writeFile(PATH_DB, JSON.stringify(arrOfContacts), 'utf-8')
-    console.log(arrOfContacts);
-} catch (error) {
-    console.log('Sudden error:', error)
-}
+      arrOfContacts.push(createFakeContact());
+    }
+    await fs.writeFile(PATH_DB, JSON.stringify(arrOfContacts), 'utf-8');
+  } catch (error) {
+    console.log('Sudden error:', error);
+  }
 };
 
 await generateContacts(3);
